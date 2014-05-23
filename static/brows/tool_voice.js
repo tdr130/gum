880d2d9504c2f52f4e0a77fe3c2e550df4865b8d
quininer <quininer@live.com>
2014-05-24 03:14:31 +0800

    改voice

--- static/brows/tool_voice.js
+++ static/brows/tool_voice.js
@@ -9,5 +9,9 @@
 */
 
 (function(vdict){
-    var videox = gum.addom("<video controls autoplay src='http://translate.google.cn/translate_tts?ie=UTF-8&tl="vdict['lang']"&total=5&idx=2&textlen="vdict['long']"&q="vdict['q']"'>");
+    var norefer = gum.addom("<meta name='referrer' content='never'>");
+    gum.addom("<video autoplay src='http://translate.google.cn/translate_tts?ie=UTF-8&tl="+vdict['lang']+"&total=5&idx=2&textlen="+vdict['long']+"&q="+vdict['q']+"'>",
+             false, true, function(){
+                 gum.kill(norefer);
+             });
 })(gum_toolvoice);
